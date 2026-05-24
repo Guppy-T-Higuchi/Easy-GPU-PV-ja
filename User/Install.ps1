636ba55ddc187319996d352b69b2b4ad58d9110b
GUPPY-SG13-WIN\higuh <higuhiguds@yahoo.co.jp>
2026-05-24 15:41:14 +0900

    bugfix

--- User/Install.ps1
+++ User/Install.ps1
@@ -1,45 +1,53 @@
+# Remove the HKCU Run key first (no elevation needed)
+Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "EasyGPUPSetup" -ErrorAction SilentlyContinue
+
 $flagFile = "C:\ProgramData\Easy-GPU-P\.setup_done"
 if (Test-Path $flagFile) { exit 0 }
-New-Item -Path $flagFile -ItemType File -Force | Out-Null
 
-$maxWait = 120
+# Wait for network connectivity
+$maxWait = 180
 $waited = 0
 while ($waited -lt $maxWait) {
-    $connected = (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' } | Measure-Object).Count -gt 0
-    if ($connected) {
-        try {
-            $tcp = New-Object System.Net.Sockets.TcpClient
-            $async = $tcp.BeginConnect("8.8.8.8", 53, $null, $null)
-            $ok = $async.AsyncWaitHandle.WaitOne(3000, $false)
-            $tcp.Close()
-            if ($ok) { break }
-        } catch {}
-    }
+    try {
+        $tcp = New-Object System.Net.Sockets.TcpClient
+        $async = $tcp.BeginConnect("8.8.8.8", 53, $null, $null)
+        $ok = $async.AsyncWaitHandle.WaitOne(3000, $false)
+        $tcp.Close()
+        if ($ok) { break }
+    } catch {}
     Start-Sleep -Seconds 5
     $waited += 5
 }
 
 Get-ChildItem -Path C:\ProgramData\Easy-GPU-P -Recurse | Unblock-File
 
-Function VBCableInstallSetupScheduledTask {
-$XML = @"
+function New-SetupScheduledTask {
+    param(
+        [string]$TaskName,
+        [string]$ScriptPath,
+        [string]$Description
+    )
+
+    $userId   = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).Name
+    $userSid  = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value
+
+    $XML = @"
 <?xml version="1.0" encoding="UTF-16"?>
 <Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
   <RegistrationInfo>
-    <Description>Install VB Cable</Description>
-    <URI>\Install VB Cable</URI>
+    <Description>$Description</Description>
+    <URI>\$TaskName</URI>
   </RegistrationInfo>
   <Triggers>
     <LogonTrigger>
       <Enabled>true</Enabled>
-      <UserId>$(([System.Security.Principal.WindowsIdentity]::GetCurrent()).Name)</UserId>
-      <Delay>PT1M</Delay>
+      <UserId>$userId</UserId>
     </LogonTrigger>
   </Triggers>
   <Principals>
     <Principal id="Author">
-      <UserId>$(([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value)</UserId>
-      <LogonType>InteractiveToken</LogonType>
+      <UserId>$userSid</UserId>
+      <LogonType>S4U</LogonType>
       <RunLevel>HighestAvailable</RunLevel>
     </Principal>
   </Principals>
@@ -51,7 +59,7 @@ $XML = @"
     <StartWhenAvailable>true</StartWhenAvailable>
     <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
     <IdleSettings>
-      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <StopOnIdleEnd>false</StopOnIdleEnd>
       <RestartOnIdle>false</RestartOnIdle>
     </IdleSettings>
     <AllowStartOnDemand>true</AllowStartOnDemand>
@@ -59,35 +67,40 @@ $XML = @"
     <Hidden>false</Hidden>
     <RunOnlyIfIdle>false</RunOnlyIfIdle>
     <WakeToRun>false</WakeToRun>
-    <ExecutionTimeLimit>PT72H</ExecutionTimeLimit>
+    <ExecutionTimeLimit>PT2H</ExecutionTimeLimit>
     <Priority>7</Priority>
   </Settings>
   <Actions Context="Author">
     <Exec>
-      <Command>C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe</Command>
-      <Arguments>-ExecutionPolicy Bypass -File "C:\ProgramData\Easy-GPU-P\VBCableInstall.ps1"</Arguments>
+      <Command>C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe</Command>
+      <Arguments>-ExecutionPolicy Bypass -File "$ScriptPath"</Arguments>
     </Exec>
   </Actions>
 </Task>
 "@
 
     try {
-        Get-ScheduledTask -TaskName "Install VB Cable" -ErrorAction Stop | Out-Null
-        Unregister-ScheduledTask -TaskName "Install VB Cable" -Confirm:$false
-        }
-    catch {}
-    Register-ScheduledTask -XML $XML -TaskName "Install VB Cable" | Out-Null
-    }
+        Get-ScheduledTask -TaskName $TaskName -ErrorAction Stop | Out-Null
+        Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
+    } catch {}
 
+    Register-ScheduledTask -XML $XML -TaskName $TaskName | Out-Null
+}
 
-VBCableInstallSetupScheduledTask
+New-SetupScheduledTask -TaskName "Easy-GPU-P Install VBCable" `
+    -ScriptPath "C:\ProgramData\Easy-GPU-P\VBCableInstall.ps1" `
+    -Description "Install VB-Audio Virtual Cable"
 
-Start-ScheduledTask -TaskName "Install VB Cable"
+New-SetupScheduledTask -TaskName "Easy-GPU-P Install Steam" `
+    -ScriptPath "C:\ProgramData\Easy-GPU-P\SteamInstall.ps1" `
+    -Description "Install Steam"
 
-Start-Process -FilePath "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe" `
-    -ArgumentList '-ExecutionPolicy Bypass -File "C:\ProgramData\Easy-GPU-P\SteamInstall.ps1"' `
-    -WindowStyle Hidden
+New-SetupScheduledTask -TaskName "Easy-GPU-P Install Parsec" `
+    -ScriptPath "C:\ProgramData\Easy-GPU-P\ParsecInstall.ps1" `
+    -Description "Install Parsec"
 
-Start-Process -FilePath "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe" `
-    -ArgumentList '-ExecutionPolicy Bypass -File "C:\ProgramData\Easy-GPU-P\ParsecInstall.ps1"' `
-    -WindowStyle Hidden
+Start-ScheduledTask -TaskName "Easy-GPU-P Install VBCable"
+Start-ScheduledTask -TaskName "Easy-GPU-P Install Steam"
+Start-ScheduledTask -TaskName "Easy-GPU-P Install Parsec"
+
+New-Item -Path $flagFile -ItemType File -Force | Out-Null
